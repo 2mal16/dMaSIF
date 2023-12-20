e2d725697a385932502864566653d65c9379cdc8
Sebastian Salzmann <bastisalzmann@hotmail.com>
2023-12-20 16:00:32 +0100

    add npy filter before dataloader

--- main_inference.py
+++ main_inference.py
@@ -41,6 +41,7 @@ elif args.pdb_list != "":
     with open(args.pdb_list) as f:
         pdb_list = f.read().splitlines()
     single_data_dir = Path("./data_preprocessing/npys/")
+    pdb_list = [pdb for pdb in pdb_list if Path(single_data_dir / f"{pdb}_atomxyz.npy").exists()]
     test_dataset = [load_protein_pair(pdb, single_data_dir,single_pdb=True) for pdb in pdb_list]
     test_pdb_ids = [pdb for pdb in pdb_list]
 else:
